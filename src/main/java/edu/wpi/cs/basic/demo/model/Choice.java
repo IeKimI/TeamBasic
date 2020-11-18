c9abd05b94df702203e5a2d516ca5f3f7bc999a3
Matthew Hlushko <Matthew Hlushko@192.168.1.243>
2020-11-18 13:57:45 -0500

    Choice progress

--- src/main/java/edu/wpi/cs/basic/demo/model/Choice.java
+++ src/main/java/edu/wpi/cs/basic/demo/model/Choice.java
@@ -1,17 +1,43 @@
 package edu.wpi.cs.basic.demo.model;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Choice {
 	public final String uniqueID = "";
-	boolean complete;
+	List<AlternativeChoice> alternativeChoices = new ArrayList<AlternativeChoice>();
+	List<TeamMember> participatingMembers = new ArrayList<TeamMember>();
+	String description;
 	Date dayOfCompletion;
+	boolean isCompleted;
+	
+	public Choice(List<AlternativeChoice> alternativeChoices, List<TeamMember> participatingMembers, Date dayOfCompletion, String description) {
+		this.alternativeChoices = alternativeChoices;
+		this.participatingMembers = participatingMembers;
+		this.dayOfCompletion = dayOfCompletion;
+		this.description = description;
+		this.isCompleted = false;
+	}
+	
+	public Choice() {
+	
+	}
+	
+	public String getReport(TeamMember member) {
+		return null;
+	}
+	
+	public boolean completeChoice(Choice choice) {
+		return false;
+	}
+	
 	
 	public boolean isComplete() {
-		return complete;
+		return isCompleted;
 	}
 	public void setComplete(boolean complete) {
-		this.complete = complete;
+		this.isCompleted = complete;
 	}
 	
 	
