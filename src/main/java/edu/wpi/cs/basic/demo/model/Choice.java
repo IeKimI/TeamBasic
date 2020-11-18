1f40b3fcfdc93764aaf3f666bd9e35bc70466018
erene <erene@Laptop>
2020-11-18 13:17:35 -0500

    Adding variables I need

--- src/main/java/edu/wpi/cs/basic/demo/model/Choice.java
+++ src/main/java/edu/wpi/cs/basic/demo/model/Choice.java
@@ -2,5 +2,14 @@ package edu.wpi.cs.basic.demo.model;
 
 public class Choice {
 	public final String uniqueID = "";
+	boolean complete;
+	
+	public boolean isComplete() {
+		return complete;
+	}
+	public void setComplete(boolean complete) {
+		this.complete = complete;
+	}
+	
 	
 }
