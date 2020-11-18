e6143c59b2b6c774fd45555d163c94b0475ccbbc
erene <erene@Laptop>
2020-11-18 13:20:25 -0500

    Added dayOfCompletion

--- src/main/java/edu/wpi/cs/basic/demo/model/Choice.java
+++ src/main/java/edu/wpi/cs/basic/demo/model/Choice.java
@@ -1,8 +1,11 @@
 package edu.wpi.cs.basic.demo.model;
 
+import java.sql.Date;
+
 public class Choice {
 	public final String uniqueID = "";
 	boolean complete;
+	Date dayOfCompletion;
 	
 	public boolean isComplete() {
 		return complete;
