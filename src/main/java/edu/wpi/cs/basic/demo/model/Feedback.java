137bebdeb737aa75f17c24cd30ea210a4e388b18
Aruana279 <aruzhan.koshkarova@gmail.com>
2020-11-19 00:24:51 +0600

    Initial edit of Feeback.java

--- src/main/java/edu/wpi/cs/basic/demo/model/Feedback.java
+++ src/main/java/edu/wpi/cs/basic/demo/model/Feedback.java
@@ -1,9 +1,19 @@
 package edu.wpi.cs.basic.demo.model;
 
-public class Feedback {
+import java.util.ArrayList;
+import java.util.List;
 
-	public Feedback() {
-		// TODO Auto-generated constructor stub
+public class Feedback {
+	final String timeStamp;
+	final String text;
+	List <TeamMember> users=new ArrayList<TeamMember>();
+
+	public Feedback(String timeS, String txt, ArrayList<TeamMember> users) {
+		super();
+		this.timeStamp=timeS;
+		this.text=txt;
+		this.users=users;
+		
 		
 	}
 
