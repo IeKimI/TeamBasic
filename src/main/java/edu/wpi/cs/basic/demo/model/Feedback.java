ef40bc09345989d2aaeebe6a088a6086d44938c4
Aruana279 <aruzhan.koshkarova@gmail.com>
2020-11-19 00:36:42 +0600

    Initial

--- src/main/java/edu/wpi/cs/basic/demo/model/Feedback.java
+++ src/main/java/edu/wpi/cs/basic/demo/model/Feedback.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Feedback {
-	final String timeStamp; 
+	final String timeStamp;
 	final String text;
 	List <TeamMember> users=new ArrayList<TeamMember>();
 
