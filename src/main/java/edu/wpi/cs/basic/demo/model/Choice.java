6ae2cf4548f8c5e6f24df628d25184c3a0b7c35b
erene <erene@Laptop>
2020-11-18 14:03:02 -0500

    A lot of changes

--- src/main/java/edu/wpi/cs/basic/demo/model/Choice.java
+++ src/main/java/edu/wpi/cs/basic/demo/model/Choice.java
@@ -6,39 +6,94 @@ import java.util.List;
 
 public class Choice {
 	public final String uniqueID = "";
-	List<AlternativeChoice> alternativeChoices = new ArrayList<AlternativeChoice>();
-	List<TeamMember> participatingMembers = new ArrayList<TeamMember>();
+	ArrayList<AlternativeChoice> alternativeChoices = new ArrayList<AlternativeChoice>();
+	ArrayList<TeamMember> participatingMembers = new ArrayList<TeamMember>();
 	String description;
 	Date dayOfCompletion;
+	float daysOld;
 	boolean isCompleted;
-	
-	public Choice(List<AlternativeChoice> alternativeChoices, List<TeamMember> participatingMembers, Date dayOfCompletion, String description) {
+
+	public ArrayList<AlternativeChoice> getAlternativeChoices() {
+		return alternativeChoices;
+	}
+
+	public void setAlternativeChoices(ArrayList<AlternativeChoice> alternativeChoices) {
+		this.alternativeChoices = alternativeChoices;
+	}
+
+	public ArrayList<TeamMember> getParticipatingMembers() {
+		return participatingMembers;
+	}
+
+	public void setParticipatingMembers(ArrayList<TeamMember> participatingMembers) {
+		this.participatingMembers = participatingMembers;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDayOfCompletion() {
+		return dayOfCompletion;
+	}
+
+	public void setDayOfCompletion(Date dayOfCompletion) {
+		this.dayOfCompletion = dayOfCompletion;
+	}
+
+	public float getDaysOld() {
+		return daysOld;
+	}
+
+	public void setDaysOld(float daysOld) {
+		this.daysOld = daysOld;
+	}
+
+	public boolean isCompleted() {
+		return isCompleted;
+	}
+
+	public void setCompleted(boolean isCompleted) {
+		this.isCompleted = isCompleted;
+	}
+
+	public String getUniqueID() {
+		return uniqueID;
+	}
+
+	public Choice(ArrayList<AlternativeChoice> alternativeChoices, ArrayList<TeamMember> participatingMembers,
+			String description, Date dayOfCompletion, float daysOld, boolean isCompleted) {
+		super();
 		this.alternativeChoices = alternativeChoices;
 		this.participatingMembers = participatingMembers;
-		this.dayOfCompletion = dayOfCompletion;
 		this.description = description;
-		this.isCompleted = false;
+		this.dayOfCompletion = dayOfCompletion;
+		this.daysOld = daysOld;
+		this.isCompleted = isCompleted;
 	}
-	
+
 	public Choice() {
-		
+
 	}
-	
+
 	public String getReport(TeamMember member) {
 		return null;
 	}
-	
+
 	public boolean completeChoice(Choice choice) {
 		return false;
 	}
-	
-	
+
 	public boolean isComplete() {
 		return isCompleted;
 	}
+
 	public void setComplete(boolean complete) {
 		this.isCompleted = complete;
 	}
-	
-	
+
 }
---
+++ src/main/java/edu/wpi/cs/basic/demo/model/Model.java
@@ -0,0 +1,26 @@
+package edu.wpi.cs.basic.demo.model;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import edu.wpi.cs.basic.demo.db.ChoiceDatabaseHandler;
+
+public class Model { // Eren
+	Hashtable<String, TeamMember> teamMembers;
+	ChoiceDatabaseHandler handler;
+	// Fill With Other Handlers Here
+
+	ArrayList<Choice> choices;
+	TeamMember loggedInUser;
+
+	public Model() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void makeChoice() {
+		if(loggedInUser!=null){
+			handler.pushChoice(new Choice());
+		}
+	}
+
+}
