eda209f19481b7d61014d732ce89101cf3d2a8bb
erene <erene@Laptop>
2020-11-18 13:21:42 -0500

    Saving

--- src/main/java/edu/wpi/cs/basic/demo/model/Admin.java
+++ src/main/java/edu/wpi/cs/basic/demo/model/Admin.java
@@ -1,5 +1,6 @@
 package edu.wpi.cs.basic.demo.model;
 
+import java.time.DayOfWeek;
 import java.util.Collection;
 import java.util.Iterator;
 
@@ -8,12 +9,22 @@ public class Admin extends TeamMember {
 	public Admin() {
 		super("ADMIN");
 	}
-	
-	String createReport(Collection<Choice> choices){
-		Iterator<Choice> iterator= choices.iterator();
-		while(iterator.hasNext()) {
-			
-		}
+
+	String createReport(Collection<Choice> choices) {
+		Iterator<Choice> iterator = choices.iterator();
+		String reportOutput = "";
+		while (iterator.hasNext()) {
+			Choice temp = iterator.next();
+			reportOutput += "Choice ID: " + temp.uniqueID + "\n";
+			reportOutput += "\t";
+			if(temp.isComplete()){
+				reportOutput+="Uncomplete";
+			}
+			else {
+				reportOutput+="Complete\n";
+				reportOutput+="\tDay Of Completion: "+ dayOfCompletion;
+			}
++		}
 		return null;
 	}
 
