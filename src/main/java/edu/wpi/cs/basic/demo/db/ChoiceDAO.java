6c64e936fd223a0154c30cfd2e7d5571c2bab6ed
Eri Kim <erikim0413@gmail.com>
2020-11-19 14:39:11 -0500

    Created a New DAO Class

---
+++ src/main/java/edu/wpi/cs/basic/demo/db/ChoiceDAO.java
@@ -0,0 +1,137 @@
+package edu.wpi.cs.basic.demo.db;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.wpi.cs.basic.demo.model.AlternativeChoice;
+import edu.wpi.cs.basic.demo.model.Choice;
+import edu.wpi.cs.basic.demo.model.TeamMember;
+
+
+public class ChoiceDAO {
+
+java.sql.Connection conn;
+	
+	final String tblName = "Choices";   // Exact capitalization
+
+    public ChoiceDAO() {
+    	try  {
+    		conn = DatabaseUtil.connect();
+    	} catch (Exception e) {
+    		conn = null;
+    	}
+    }
+
+    public Choice getChoice(String uniqueID) throws Exception {
+
+		try {
+			Choice choice = null;
+			PreparedStatement ps = conn.prepareStatement("SELECT * FROM " + tblName + " WHERE uniqueID=?;");
+			ps.setString(1, uniqueID);
+			ResultSet resultSet = ps.executeQuery();
+
+			while (resultSet.next()) {
+				choice = generateChoice(resultSet);
+			}
+			resultSet.close();
+			ps.close();
+
+			return choice;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new Exception("Failed in getting choice: " + e.getMessage());
+		}
+	}
+    
+    public boolean updateChoice(Choice Choice) throws Exception {
+        try {
+        	String query = "UPDATE " + tblName + " SET value=? WHERE name=?;";
+        	PreparedStatement ps = conn.prepareStatement(query);
+            ps.setString(1, Choice.uniqueID);
+            ps.setString(2, Choice.alternativeID);
+            int numAffected = ps.executeUpdate();
+            ps.close();
+            
+            return (numAffected == 1);
+        } catch (Exception e) {
+            throw new Exception("Failed to update report: " + e.getMessage());
+        }
+    }
+    
+    public boolean deleteChoice(Choice Choice) throws Exception {
+        try {
+            PreparedStatement ps = conn.prepareStatement("DELETE FROM " + tblName + " WHERE name = ?;");
+            ps.setString(1, Choice.name);
+            int numAffected = ps.executeUpdate();
+            ps.close();
+            
+            return (numAffected == 1);
+
+        } catch (Exception e) {
+            throw new Exception("Failed to insert Choice: " + e.getMessage());
+        }
+    }
+
+
+    public boolean addChoice(Choice Choice) throws Exception {
+        try {
+            PreparedStatement ps = conn.prepareStatement("SELECT * FROM " + tblName + " WHERE name = ?;");
+            ps.setString(1, Choice.uniqueID);
+            ResultSet resultSet = ps.executeQuery();
+            
+            // already present?
+            while (resultSet.next()) {
+                Choice c = generateChoice(resultSet);
+                resultSet.close();
+                return false;
+            }
+
+            ps = conn.prepareStatement("INSERT INTO " + tblName + " (name,value) values(?,?);");
+            ps.setString(1,  Choice.uniqueID);
+            ps.setDouble(2,  Choice.value);
+            ps.execute();
+            return true;
+
+        } catch (Exception e) {
+            throw new Exception("Failed to insert Choice: " + e.getMessage());
+        }
+    }
+
+    public List<Choice> getAllChoices() throws Exception {
+        
+        List<Choice> allChoices = new ArrayList<>();
+        try {
+            Statement statement = conn.createStatement();
+            String query = "SELECT * FROM " + tblName + ";";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                Choice c = generateChoice(resultSet);
+                allChoices.add(c);
+            }
+            resultSet.close();
+            statement.close();
+            return allChoices;
+
+        } catch (Exception e) {
+            throw new Exception("Failed in getting Choices: " + e.getMessage());
+        }
+    }
+    
+    private Choice generateChoice(ResultSet resultSet) throws Exception {
+        String uniqueID  = resultSet.getString("name");
+        ArrayList<AlternativeChoice> alternativeChoices;
+		ArrayList<TeamMember> participatingMembers;
+		String description;
+		Date dayOfCompletion;
+		float daysOld;
+		boolean isCompleted;
+		return new Choice(uniqueID, alternativeChoices, participatingMembers, description, dayOfCompletion, daysOld,
+				isCompleted);
+    }
+}
