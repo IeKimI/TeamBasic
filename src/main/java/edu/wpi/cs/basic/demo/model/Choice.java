6a568996f2aabcc91115863c2a438059fa98a4d6
Matthew Hlushko <Matthew Hlushko@130.215.170.114>
2020-11-20 12:54:21 -0500

    actual updated constructor oops lol

--- src/main/java/edu/wpi/cs/basic/demo/model/Choice.java
+++ src/main/java/edu/wpi/cs/basic/demo/model/Choice.java
@@ -19,7 +19,7 @@ public class Choice {
 
 	public Choice(String uniqueID, ArrayList<AlternativeChoice> alternativeChoices,
 			ArrayList<TeamMember> participatingMembers, String description, Date dateOfCompletion, Date dateOfCreation,
-			boolean isCompleted) {
+			boolean isCompleted, int maxNumOfTeamMembers) {
 		this.uniqueID = uniqueID;
 		this.alternativeChoices = alternativeChoices;
 		this.participatingMembers = participatingMembers;
@@ -27,6 +27,7 @@ public class Choice {
 		this.dateOfCompletion = dateOfCompletion;
 		this.dateOfCreation = dateOfCreation;
 		this.isCompleted = isCompleted;
+		this.maxNumOfTeamMembers = maxNumOfTeamMembers;
 	}
 	
 	public Choice(String uniqueID) {
@@ -44,7 +45,12 @@ public class Choice {
 	} 
 
 	public void setAlternativeChoices(ArrayList<AlternativeChoice> alternativeChoices) {
-		this.alternativeChoices = alternativeChoices;
+		if(alternativeChoices.size() < 6 || alternativeChoices.size() > 1) {
+			this.alternativeChoices = alternativeChoices;
+		}
+		else {
+			// must display error message
+		}
 	}
 
 	public ArrayList<TeamMember> getParticipatingMembers() {
@@ -52,7 +58,12 @@ public class Choice {
 	}
 
 	public void setParticipatingMembers(ArrayList<TeamMember> participatingMembers) {
-		this.participatingMembers = participatingMembers;
+		if(participatingMembers.size() < this.maxNumOfTeamMembers) {
+			this.participatingMembers = participatingMembers;
+		}
+		else {
+			// must have error message
+		}
 	}
 
 	public String getUniqueID() {
