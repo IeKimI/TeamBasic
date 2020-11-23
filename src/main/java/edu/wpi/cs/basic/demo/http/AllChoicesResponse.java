4d35d69fabfd9c902d5765613f410660cc9903af
Eri Kim <erikim0413@gmail.com>
2020-11-22 22:01:48 -0500

    ListAllChoicesHandler and AllChoicesResponse Created

---
+++ src/main/java/edu/wpi/cs/basic/demo/http/AllChoicesResponse.java
@@ -0,0 +1,31 @@
+package edu.wpi.cs.basic.demo.http;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.wpi.cs.basic.demo.model.Choice;
+
+
+public class AllChoicesResponse {
+	public final List<Choice> list;
+	public final int statusCode;
+	public final String error;
+	
+	public AllConstantsResponse (List<Choice> list, int code) {
+		this.list = list;
+		this.statusCode = code;
+		this.error = "";
+	}
+	
+	public AllConstantsResponse (int code, String errorMessage) {
+		this.list = new List<Choice>();
+		this.statusCode = code;
+		this.error = errorMessage;
+	}
+	
+	public String toString() {
+		if (list == null) { return "EmptyConstants"; }
+		return "AllConstants(" + list.size() + ")";
+	}
+
+}
