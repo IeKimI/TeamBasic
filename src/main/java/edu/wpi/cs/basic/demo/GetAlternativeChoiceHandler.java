f629c6f542415828949b291ab1a8872faa26ac1e
erene <erene@Laptop>
2020-11-24 19:46:37 -0500

    Changed getAlternativeChoice to return a list instead of a single alternative. Pass in a choice ID and the respective alternatives will be returned

--- src/main/java/edu/wpi/cs/basic/demo/GetAlternativeChoiceHandler.java
+++ src/main/java/edu/wpi/cs/basic/demo/GetAlternativeChoiceHandler.java
@@ -1,5 +1,7 @@
 package edu.wpi.cs.basic.demo;
 
+import java.util.List;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -9,28 +11,29 @@ import edu.wpi.cs.basic.demo.db.ChoiceDAO;
 import edu.wpi.cs.basic.demo.model.AlternativeChoice;
 import edu.wpi.cs.basic.demo.model.Choice;
 
-public class GetAlternativeChoiceHandler implements RequestHandler<String, AlternativeChoice> {
+public class GetAlternativeChoiceHandler implements RequestHandler<String, List<AlternativeChoice>> {
 	LambdaLogger logger;
+
 	public GetAlternativeChoiceHandler() {
 		// TODO Auto-generated constructor stub
 	}
 
-	AlternativeChoice getAlternativeChoice(String altID) throws Exception {
+	List<AlternativeChoice> getAlternativeChoices(String choiceID) throws Exception {
 		if (logger != null)
 			logger.log("getChoice has been called.");
 		AlternativeChoiceDAO database = new AlternativeChoiceDAO();
-		AlternativeChoice request = database.getAlternativeChoice(altID);
+		List<AlternativeChoice> request = database.getAlternativeChoice(choiceID);
 		if (logger != null)
 			logger.log("The choice has been got.");
 		return request;
 	}
-	
-	public AlternativeChoice handleRequest(String altID, Context c) {
+
+	public List<AlternativeChoice> handleRequest(String altID, Context c) {
 		logger = c.getLogger();
 		logger.log("Getting Choice " + altID);
 
 		try {
-			return getAlternativeChoice(altID);
+			return getAlternativeChoices(altID);
 		} catch (Exception e) {
 			logger.log("An exception was caught in the handleRequest when getting the choice.");
 			return null;
