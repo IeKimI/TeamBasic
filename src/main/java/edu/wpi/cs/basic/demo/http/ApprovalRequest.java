ef8d90c17d6354f4eb66de739d07c74296bd0d62
Matthew Hlushko <Matthew Hlushko@DESKTOP-OAKP5I1.fios-router.home>
2020-11-30 21:30:45 -0500

    Approval Request and Approval Response

--- src/main/java/edu/wpi/cs/basic/demo/http/ApprovalRequest.java
+++ src/main/java/edu/wpi/cs/basic/demo/http/ApprovalRequest.java
@@ -1,5 +1,48 @@
 package edu.wpi.cs.basic.demo.http;
 
-public class ApprovalRequest {
+import java.util.ArrayList;
+import java.util.List;
 
+import edu.wpi.cs.basic.demo.model.AlternativeChoice;
+import edu.wpi.cs.basic.demo.model.TeamMember;
+
+public class ApprovalRequest {
+//matt
+	int alternativeID;
+	int numOfApprovals;
+	List<TeamMember> listOfTeamMembers;
+	
+	public int getAlternativeID() {
+		return alternativeID;
+	}
+	public void setAlternativeID(int alternativeID) {
+		this.alternativeID = alternativeID;
+	}
+	public int getNumOfApprovals() {
+		return numOfApprovals;
+	}
+	public void setNumOfApprovals(int numOfApprovals) {
+		this.numOfApprovals = numOfApprovals;
+	}
+	public List<TeamMember> getListOfTeamMembers() {
+		return listOfTeamMembers;
+	}
+	public void setListOfTeamMembers(List<TeamMember> listOfTeamMembers) {
+		this.listOfTeamMembers = listOfTeamMembers;
+	}
+	
+	public ApprovalRequest(int alternativeID, int numOfApprovals, List<TeamMember> listOfTeamMembers) {
+		this.alternativeID = alternativeID;
+		this.numOfApprovals = numOfApprovals;
+		this.listOfTeamMembers = listOfTeamMembers;
+	}
+	
+	public ApprovalRequest() {
+		
+	}
+	
+	
+	
+	
+	
 }
--- src/main/java/edu/wpi/cs/basic/demo/http/ApprovalResponse.java
+++ src/main/java/edu/wpi/cs/basic/demo/http/ApprovalResponse.java
@@ -1,5 +1,23 @@
 package edu.wpi.cs.basic.demo.http;
 
 public class ApprovalResponse {
+	//matt
+	public final String response;
+	public final int httpCode;
+	
+	public ApprovalResponse (String s, int code) {
+		this.response = s;
+		this.httpCode = code;
+	}
+	
+	// 200 means success
+	public ApprovalResponse (String s) {
+		this.response = s;
+		this.httpCode = 200;
+	}
+	
+	public String toString() {
+		return "Response(" + response + ")";
+	}
 
 }
