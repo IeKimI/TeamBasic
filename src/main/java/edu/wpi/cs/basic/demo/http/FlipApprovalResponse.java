b9576e4a174b22527d303f635254f873e04c5085
Matthew Hlushko <Matthew Hlushko@DESKTOP-OAKP5I1.fios-router.home>
2020-12-01 21:00:32 -0500

    ee

--- src/main/java/edu/wpi/cs/basic/demo/http/FlipApprovalResponse.java
+++ src/main/java/edu/wpi/cs/basic/demo/http/FlipApprovalResponse.java
@@ -15,24 +15,11 @@ public class FlipApprovalResponse {
 	public boolean hasBeenDisapproved;
 	public int httpCode;
 	
-	public FlipApprovalResponse(String response, ApprovalInfo approvInfo) {
+	public FlipApprovalResponse(String response, ApprovalInfo approvInfo, boolean hasBeenApproved, boolean hasBeenDisapproved) {
 		this.response = response;
 		this.httpCode = 200;
-	}
-	
-	//when code not 200 null is returned as the choice
-	public FlipApprovalResponse(int code, String response) {
-		this.approvInfo = null;
-		this.response = response;
-		this.httpCode = code;
-		}
-	
-	// 200 means success
-	public FlipApprovalResponse (String s, int numVotes, List<TeamMember> ListOfTeamMember) {
-		this.response = s;
-		this.httpCode = 200;
-		this.numVotes = numVotes;
-		this.ListOfTeamMember = ListOfTeamMember;
+		this.hasBeenApproved = hasBeenApproved;
+		this.hasBeenDisapproved = hasBeenDisapproved;
 	}
 	
 	
