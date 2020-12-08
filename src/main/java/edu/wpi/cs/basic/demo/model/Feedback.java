ff09fb9f111f79eef5e5dd17c48b514423ffa5ed
erene <erene@Laptop>
2020-12-08 13:47:30 -0500

    Finished Feedback DAO except for delete Feedback, Will do that later

--- src/main/java/edu/wpi/cs/basic/demo/model/Feedback.java
+++ src/main/java/edu/wpi/cs/basic/demo/model/Feedback.java
@@ -2,7 +2,7 @@ package edu.wpi.cs.basic.demo.model;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.List; 
+import java.util.List;
 
 public class Feedback {
 	final Timestamp timeStamp;
@@ -10,21 +10,60 @@ public class Feedback {
 	int teamMemberID;
 	int alternativeChoiceID;
 	int feedbackID;
+
+	public Feedback(String text, int teamMemberID, int alternativeChoiceID, int feedbackID) {
+		this.timeStamp = new Timestamp(System.currentTimeMillis());
+		this.text = text;
+		this.teamMemberID = teamMemberID;
+		this.alternativeChoiceID = alternativeChoiceID;
+		this.feedbackID = feedbackID;
+	}
+
+	public Feedback(String text, int teamMemberID, int alternativeChoiceID) {
+		this.timeStamp = new Timestamp(System.currentTimeMillis());
+		this.text = text;
+		this.teamMemberID = teamMemberID;
+		this.alternativeChoiceID = alternativeChoiceID;
+	}
+
 	public Feedback(Timestamp timeStamp, String text, int teamMemberID, int alternativeChoiceID, int feedbackID) {
-		super();
 		this.timeStamp = timeStamp;
 		this.text = text;
 		this.teamMemberID = teamMemberID;
 		this.alternativeChoiceID = alternativeChoiceID;
 		this.feedbackID = feedbackID;
 	}
-	public Feedback(Timestamp timeStamp, String text, int teamMemberID, int alternativeChoiceID) {
-		super();
-		this.timeStamp = timeStamp;
-		this.text = text;
+
+	public int getTeamMemberID() {
+		return teamMemberID;
+	}
+
+	public void setTeamMemberID(int teamMemberID) {
 		this.teamMemberID = teamMemberID;
+	}
+
+	public int getAlternativeChoiceID() {
+		return alternativeChoiceID;
+	}
+
+	public void setAlternativeChoiceID(int alternativeChoiceID) {
 		this.alternativeChoiceID = alternativeChoiceID;
 	}
 
+	public int getFeedbackID() {
+		return feedbackID;
+	}
+
+	public void setFeedbackID(int feedbackID) {
+		this.feedbackID = feedbackID;
+	}
+
+	public Timestamp getTimeStamp() {
+		return timeStamp;
+	}
+
+	public String getText() {
+		return text;
+	}
 
 }
