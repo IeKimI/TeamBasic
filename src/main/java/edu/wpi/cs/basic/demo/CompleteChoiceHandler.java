f645dfaf84c4b8166167b0b749cb4833f7354a48
Aruana279 <aruzhan.koshkarova@gmail.com>
2020-12-08 22:11:24 +0600

    created CompleteChoiceHandler and request/response files

---
+++ src/main/java/edu/wpi/cs/basic/demo/CompleteChoiceHandler.java
@@ -0,0 +1,30 @@
+package edu.wpi.cs.basic.demo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import edu.wpi.cs.basic.demo.http.CompleteChoiceRequest;
+import edu.wpi.cs.basic.demo.http.CompleteChoiceResponse;
+import edu.wpi.cs.basic.demo.http.CreateChoiceRequest;
+import edu.wpi.cs.basic.demo.http.CreateChoiceResponse;
+
+public class CompleteChoiceHandler implements RequestHandler<CompleteChoiceRequest, CompleteChoiceResponse>{
+	
+	LambdaLogger logger;
+
+	@Override
+	public CompleteChoiceResponse handleRequest(CompleteChoiceRequest input, Context context) {
+		logger = context.getLogger();
+		CompleteChoiceResponse response;
+		if (logger != null) {
+			logger.log("in CompleteChoice");
+		}
+		return null;
+		
+		
+	}
+
+	
+
+}
