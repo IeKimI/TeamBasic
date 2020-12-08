cf122889c9184988956e711d66cd9ccd69b34a54
Eri Kim <erikim0413@gmail.com>
2020-12-08 11:11:56 -0500

    Started cleaning up some code

--- src/main/java/edu/wpi/cs/basic/demo/CreateChoiceHandler.java
+++ src/main/java/edu/wpi/cs/basic/demo/CreateChoiceHandler.java
@@ -28,80 +28,15 @@ public class CreateChoiceHandler implements RequestHandler<CreateChoiceRequest, 
 //
 	private int id = 0;
 
-	public String getNextID() {
-		// Can through database for earliest available ID
-		return (new Integer(id++)).toString();
-	}
 
-//
-//	public boolean pushChoice(Choice choice) {
-//		return false;
-//	}
-//
-//	@Override
-//	public CreateChoiceResponse handleRequest(CreateChoiceRequest input, Context context) {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
+
 
 	LambdaLogger logger;
 
 	// To access S3 storage
 	private AmazonS3 s3 = null;
 
-	// Note: this works, but it would be better to move this to
-	// environment/configuration mechanisms
-	// which you don't have to do for this project.
-	// public static final String REAL_BUCKET = "constants/";
 
-	/**
-	 * Store into RDS.
-	 * 
-	 * @throws Exception
-	 */
-//	public boolean createChoice(String description, int maxNumTeamMember,
-//			ArrayList<AlternativeChoice> alternatives) throws Exception {
-//		if (logger != null) {
-//			logger.log("in createChoice");
-//		}
-//		String uniqueID = Integer.toString(description.hashCode()+alternatives.get(1).getDescription().hashCode());
-//		ChoiceDAO choiceDao = new ChoiceDAO();
-//		/**
-//		 * public Choice(String uniqueID, ArrayList<AlternativeChoice>
-//		 * alternativeChoices, ArrayList<TeamMember> participatingMembers, String
-//		 * description, Date dateOfCompletion, Date dateOfCreation, boolean isCompleted,
-//		 * int maxNumOfTeamMembers) { this.maxNumOfTeamMembers = maxNumOfTeamMembers;
-//		 * this.uniqueID = uniqueID; this.setAlternativeChoices(alternativeChoices);
-//		 * this.setParticipatingMembers(participatingMembers); this.description =
-//		 * description; this.dateOfCompletion = dateOfCompletion; this.dateOfCreation =
-//		 * dateOfCreation; this.isCompleted = isCompleted; }
-//		 */
-//		// check if present
-//		Choice exist = choiceDao.getChoice(uniqueID);
-//
-//		// go through alternatives and put each in alternativeDAO
-//		AlternativeChoiceDAO alternativeDAO = new AlternativeChoiceDAO();
-//
-//		for (AlternativeChoice alt : alternatives) {
-//			alt.setChoiceID(uniqueID);
-//			alt.setDescription(alt.getDescription());
-//			alternativeDAO.addAlternative(alt);
-//		}
-//		Choice choice = new Choice(uniqueID, AlternativeChoiceDAO.getAllAlternatives(uniqueID),
-//				new ArrayList<TeamMember>(maxNumTeamMember), description, null, null, // Change to an array instead of
-//																						// an arrayList
-//				new java.sql.Date(System.currentTimeMillis()), false);
-//		if (exist == null) {
-//			return choiceDao.addChoice(choice);
-//		} else {
-//			return false;
-//		}
-//	}
-//	//Overloaded function
-//	public boolean createChoice(Choice c) throws Exception {
-//		return createChoice(c.getDescription(), c.getMaxNumOfTeamMembers(), c.getAlternativeChoices());
-//
-//	}
 
 	boolean createChoice(String uniqueID, CreateChoiceRequest req) throws Exception {
 		if (logger != null) {
@@ -127,49 +62,11 @@ public class CreateChoiceHandler implements RequestHandler<CreateChoiceRequest, 
 		return true;
 	}
 
-//	/** Create S3 bucket
-//	 * 
-//	 * @throws Exception 
-//	 */
-//	boolean createSystemChioce(String uniqueID, ArrayList<AlternativeChoice> alternatives, ArrayList<TeamMember> teamMembers, String description, Date dateOfCompletion, float daysOld, boolean isCompleted) throws Exception {
-//		if (logger != null) { logger.log("in createSystemConstant"); }
-//		
-//		if (s3 == null) {
-//			logger.log("attach to S3 request");
-//			s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_2).build();
-//			logger.log("attach to S3 succeed");
-//		}
-//
-//		String folder = BucketManager.getChoiceFolder() + "/";
-//		
-//		byte[] contents = ("" + alternatives.get(1)).getBytes();
-//		ByteArrayInputStream bais = new ByteArrayInputStream(contents);
-//		ObjectMetadata omd = new ObjectMetadata();
-//		omd.setContentLength(contents.length);
-//		
-//		// makes the object publicly visible
-//		PutObjectResult res = s3.putObject(new PutObjectRequest(BucketManager.bucket, folder + uniqueID, bais, omd)
-//				.withCannedAcl(CannedAccessControlList.PublicRead));
-//		
-//		// if we ever get here, then whole thing was stored
-//		return true;
-//	}
 
-	/** Here primarily to clean up testing. */
-	void deleteSystemChoice(float daysOld) {
-		// TODO: add logic for replacing deleted id numbers
-		if (s3 == null) {
-			logger.log("attach to S3 request");
-			s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_2).build();
-			logger.log("attach to S3 succeed");
-		}
-		String folder = BucketManager.getChoiceFolder() + "/";
-		s3.deleteObject(new DeleteObjectRequest(BucketManager.bucket, folder + daysOld));
-	}
 
 	String createUniqueChoiceID(CreateChoiceRequest req) {
 		String uniqueID = Integer.toString(Math.abs(
-				req.getDescription().hashCode() + req.getAlternativeChoices().get(1).getDescription().hashCode()));
+				req.getDescription().hashCode() + req.getAlternativeChoices().get(1).getDescription().hashCode()*req.getAlternativeChoices().get(2).getDescription().hashCode()));
 		return uniqueID;
 	}
 
