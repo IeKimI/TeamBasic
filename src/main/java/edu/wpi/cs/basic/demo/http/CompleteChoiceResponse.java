edcb019063bc7784fcabb9edebd707092789eac7
Aruana279 <aruzhan.koshkarova@gmail.com>
2020-12-09 02:01:10 +0600

    finished CompleteChoiceRequest and CompleteChoiceResponse.

--- src/main/java/edu/wpi/cs/basic/demo/http/CompleteChoiceResponse.java
+++ src/main/java/edu/wpi/cs/basic/demo/http/CompleteChoiceResponse.java
@@ -1,5 +1,24 @@
 package edu.wpi.cs.basic.demo.http;
 
+import java.util.List;
+
+import edu.wpi.cs.basic.demo.model.Choice;
+
 public class CompleteChoiceResponse {
+	public boolean isComplete;
+	public final int statusCode;
+	public final String error;
+
+	public CompleteChoiceResponse (int statusCode) {
+		super();
+		this.statusCode = statusCode;
+		this.error = "";
+	}
+
+	public CompleteChoiceResponse ( String errorMessage, int statusCode) {
+		super();
+		this.statusCode = statusCode;
+		this.error = errorMessage;
+	}
 
 }
