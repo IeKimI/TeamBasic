7d4a1474955e62c006450c0afb71b352ba37f741
Eri Kim <erikim0413@gmail.com>
2020-12-08 22:17:49 -0500

    Trying to make the website look better...

--- js/altInfo.js
+++ js/altInfo.js
@@ -44,31 +44,31 @@ function processAlternatives(result) {
 	var count = 0;
 	var realCount = 0;
 	var approvalButtons = "";
-	var disapprovalButtons ="";
+	var disapprovalButtons = "";
 
 	for (var i in js) {
 		var alternative = js[i];
 		var alternativeID = alternative["alternativeID"];
 		count = count + 1;
-		
-		approvalButtons = "<br><br><a onClick=\"changeImage("  + alternativeID +")\"\n>\t<img value=" + realCount.toString() + " src=\"check-mark_b&w.png\" width=\"28\" height=\"28\" border=\"0\" alt=\"javascript button\">\n</a>";
+
+		approvalButtons = "<br><br><a onClick=\"changeImage(" + alternativeID + ")\"\n>\t<img value=" + realCount.toString() + " src=\"check-mark_b&w.png\" width=\"28\" height=\"28\" border=\"0\" alt=\"javascript button\">\n</a>";
 		realCount = realCount + 1;
-		disapprovalButtons = "<a onClick=\"changeImageDisapproval("  + alternativeID +")\"\n>\t<img value=" + realCount.toString() + " src=\"cancel.png\" width=\"28\" height=\"28\" border=\"0\" alt=\"javascript button\">\n</a>";
+		disapprovalButtons = "<a onClick=\"changeImageDisapproval(" + alternativeID + ")\"\n>\t<img value=" + realCount.toString() + " src=\"cancel.png\" width=\"28\" height=\"28\" border=\"0\" alt=\"javascript button\">\n</a>";
 		realCount = realCount + 1;
 		let alternativeDesc = alternative["description"];
 		console.log(alternativeDesc)
 		if (alternativeDesc != "") {
-			var isLoggedIn = window.location.href ;
+			var isLoggedIn = window.location.href;
 			isLoggedIn = isLoggedIn.split("teamMemberID=")[1];
-			if(isLoggedIn != "0"){
-				output = output + "<h4 style=\"font-family:verdana\"> Alternative " + count + ": </h4><label style=\"font-family:verdana; font-size:40px\">" + alternativeDesc + "</label> "  + "<div id=\"approvals" + count + "\">" +approvalButtons +"<label style=\"font-family:verdana\" id = \"nameList" + count + "\">Loading...</label><br><label style=\"font-family:verdana\" id = \"num" + count + "\"> "  + "</label><br><br>"+ "</div>" + "<div id=\"disapprovals" + count + "\">" +disapprovalButtons + "<label style=\"font-family:verdana\" id = \"nameDisList" + count + "\">Loading... "  + "</label><br><label style=\"font-family:verdana\" id = \"numDis" + count + "\"> "  + "</label><br><br>" + "</div>"+ "<div id=\"feedback" + count + "\"></div>";
+			if (isLoggedIn != "0") {
+				output = output + "<br><div id = alternative" + count + " class = \"myDiv\"><h4 style=\"font-family:verdana\"> Alternative " + count + ": </h4><label style=\"font-family:verdana; padding: 5px; font-size:20px\">" + "	" + alternativeDesc + "</label> " + "<div id=\"approvals" + count + "\">" + approvalButtons + "<label style=\"font-family:verdana; padding: 5px\" id = \"nameList" + count + "\">Loading...</label><br><label style=\"font-family:verdana; padding: 8px\" id = \"num" + count + "\"> " + "</label><br><br>" + "</div>" + "<div id=\"disapprovals" + count + "\">" + disapprovalButtons + "<label style=\"font-family:verdana; padding: 5px\" id = \"nameDisList" + count + "\">Loading... " + "</label><br><label style=\"font-family:verdana; padding: 8px\" id = \"numDis" + count + "\"> " + "</label><br><br>" + "</div>" + "<div id=\"feedback" + count + "\"></div></div><br>";
 			}
-			else{
-				output = output + "<h4 style=\"font-family:verdana\"> Alternative " + count + ": </h4><label style=\"font-family:verdana; font-size:40px\">" + alternativeDesc + "</label> <br>" + "<div id=\"approvals" + count + "\"></div>";
+			else {
+				output = output + "<h4 style=\"font-family:verdana\"> Alternative " + count + ": </h4><label style=\"font-family:verdana; padding: 5px; font-size:20px\">" + "	" + alternativeDesc + "</label> <br>" + "<div id=\"approvals" + count + "\"></div>";
 			}
 			console.log(output);
 		}
-		 
+
 
 	}
 	alternatives.innerHTML = output;
