cd8cb0da3f655dd97c85dc72feb26e5bc1fb06c4
Eri Kim <erikim0413@gmail.com>
2020-12-09 17:14:09 -0500

    Created a function that checks whether the choice is completed or not

--- src/main/java/edu/wpi/cs/basic/demo/GetCompleteChoiceHandler.java
+++ src/main/java/edu/wpi/cs/basic/demo/GetCompleteChoiceHandler.java
@@ -5,14 +5,16 @@ import java.util.List;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import edu.wpi.cs.basic.demo.db.ChoiceDAO;
 import edu.wpi.cs.basic.demo.db.FeedbackDAO;
 import edu.wpi.cs.basic.demo.http.CompleteChoiceResponse;
+import edu.wpi.cs.basic.demo.http.GetChoiceResponse;
 import edu.wpi.cs.basic.demo.http.GetFeedbackResponse;
 import edu.wpi.cs.basic.demo.model.Feedback;
 
-public class GetCompleteChoiceHandler {
+public class GetCompleteChoiceHandler implements RequestHandler<String, CompleteChoiceResponse>{
 	LambdaLogger logger;
 
 	public CompleteChoiceResponse handleRequest(String choiceID, Context context) {
